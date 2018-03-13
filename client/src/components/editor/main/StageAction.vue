2dff9016850655d299b7139869b17415c806c24f
Marcel Tielman <mtielman@yellowmind.nl>
2018-03-13 14:11:53 +0100

    Add Stage Action

---
+++ client/src/components/editor/main/StageAction.vue
@@ -0,0 +1,16 @@
+<script>
+// import { mapState, mapMutations } from 'vuex'
+// import { _clearSelectedElements, _addSelectedElement } from '@/store/types'
+
+import MrEl from '@/components/editor/common/mr-vue/MrEl'
+// import StageEl from './StageEl'
+
+export default {
+  name: 'stage-action',
+  props: ['elem'],
+  components: { MrEl },
+  render: function (createElement) {
+    return createElement('h1', 'A headline')
+  }
+}
+</script>
